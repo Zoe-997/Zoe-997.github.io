9728e15c279cf13b966fc9a4e8d66f3201c98948
Zoe-997 <thuminh10011997@mail.com>
2024-01-10 17:30:59 +0700

    change layout

--- app/components/Header/mainMenu.tsx
+++ app/components/Header/mainMenu.tsx
@@ -1,19 +1,50 @@
 'use client'
+import { useState } from 'react';
 import { Menu } from 'antd';
+import type { MenuProps } from 'antd';
+import Link from 'next/link';
 
 const MainMenu = () => {
-    const items = new Array(15).fill(null).map((_, index) => ({
-        key: index + 1,
-        label: `nav ${index + 1}`,
-    }));
+    const [current, setCurrent] = useState('home');
+    const items: MenuProps['items'] = [
+        {
+            key: 'home',
+            label: (
+                <Link href='/'>Home</Link>
+            )
+        },
+        {
+            key: 'our-work',
+            label: (
+                <Link href='/work'>Our work</Link>
+            )
+        },
+        {
+            key: 'about-us',
+            label: (
+                <Link href='/about'>About us</Link>
+            )
+        },
+        {
+            key: 'contact',
+            label: (
+                <Link href='/contact'>Contact</Link>
+            )
+        }
+    ]
+
+    const handleChangeActive: MenuProps['onClick'] = (e) => {
+        setCurrent(e.key)
+    }
 
     return (
         <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
+            selectedKeys={[current]}
             items={items}
             style={{ flex: 1, minWidth: 0 }}
+            onClick={handleChangeActive}
         />
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,7 @@
-'use client'
-import { Layout, theme } from 'antd';
-
-import Header from '@/app/components/Header/header'
-import Footer from '@/app/components/Footer/footer'
-import BreadcrumbComponent from '@/app/components/Common/breadcrumb'
-
-const { Content } = Layout;
-
-export default function Home() {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
-  
+export default function Home() {  
   return (
-    <Layout>
-        <Header />
-        <Content style={{ padding: '0 48px' }}>
-          <BreadcrumbComponent/>
-          <div
-            style={{
-              background: colorBgContainer,
-              minHeight: 280,
-              padding: 24,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Welcome to Zoe-997 page
-          </div>
-        </Content>
-        <Footer/>
-      </Layout>
+    <>
+      Home page
+    </>
   )
 }
