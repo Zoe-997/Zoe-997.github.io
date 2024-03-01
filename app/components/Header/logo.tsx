100614156daa361a094331fe0b5f11964a5b50b8
Zoe-997 <thuminh10011997@mail.com>
2024-03-01 14:50:42 +0700

    update logo

--- app/components/Header/logo.tsx
+++ app/components/Header/logo.tsx
@@ -1,6 +1,10 @@
+import { SmileOutlined } from '@ant-design/icons'
+
 const Logo = () => {
     return (
-        <div className="w-[120px] min-w-[120px] h-[32px] bg-[rgba(225,_225,_225,_0.2)] rounded-[6px] mr-[24px]" />
+        <div className="w-[120px] min-w-[120px] h-[32px] leading-[32px] bg-[rgba(225,_225,_225,_0.2)] rounded-[6px] mr-[24px] text-center text-white">
+            <SmileOutlined /> ZoeTrinh
+        </div>
     );
 }
  
